2d90ea542378eeedb276af79dda67a04d7d9c604
anibalsolon <anibalsolon@gmail.com>
2018-11-03 19:40:43 -0400

    update setup scripts

--- setup.py
+++ setup.py
@@ -1,27 +1,64 @@
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha', 'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: POSIX :: Linux',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7', 'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Information Analysis'
+]
+
+description = ''
+
+long_description = """========================================================
+Matchgroup
+========================================================
+
+"""
+
+__packagename__ = 'matchgroup'
+__author__ = __maintainer__ = 'Anibal Solon'
+__email__ = 'anibalsolon@gmail.com'
+__license__ = 'Apache License, 2.0'
+__status__ = 'Pre-Alpha'
+__description__ = description
+__longdesc__ = long_description
+__url__ = 'https://github.com/anibalsolon/matchgroup'
+
+DOWNLOAD_URL = (
+    'http://github.com/anibalsolon/{name}/archives/{__version__}.tar.gz'.format(
+        name=__packagename__, __version__="{__version__}"))
+
+PLATFORMS = 'OS Independent'
+PROVIDES = ['matchgroup']
+REQUIRES = [
+    'pandas>=0.23.0',
+    'numpy>=0.15.1',
+]
+
+SETUP_REQUIRES = ['setuptools>=27.0']
+TESTS_REQUIRES = ['pytest-cov', 'codecov', 'pytest-env', 'pytest-xdist']
+LINKS_REQUIRES = []
+
+EXTRA_REQUIRES = {
+    'tests': TESTS_REQUIRES,
+    'dev': TESTS_REQUIRES + ['yapf>=0.22'],
+}
+
+EXTRA_REQUIRES['all'] = list(set(sum(EXTRA_REQUIRES.values(), [])))
+
+
 def main():
+
     import os
     from setuptools import setup, find_packages
     from inspect import getfile, currentframe
+    
     import versioneer
-    from matchgroup.__about__ import (
-        __packagename__,
-        __version__,
-        __author__,
-        __email__,
-        __maintainer__,
-        __license__,
-        __description__,
-        __longdesc__,
-        __url__,
-        DOWNLOAD_URL,
-        CLASSIFIERS,
-        PROVIDES,
-        REQUIRES,
-        SETUP_REQUIRES,
-        LINKS_REQUIRES,
-        TESTS_REQUIRES,
-        EXTRA_REQUIRES,
-    )
 
     pkg_data = {'matchgroup': []}
     root_dir = os.path.dirname(os.path.abspath(getfile(currentframe())))
